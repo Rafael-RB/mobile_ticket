88b3b076d8193cd16039df623049ff5a43e0caeb
EllieFranca <franca.ems@gmail.com>
2025-11-30 15:56:51 +0000

    Inicio do sistema de criação de senhas

---
+++ ticket/src/app/services/senhas.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SenhasService } from './senhas';
+
+describe('Senhas', () => {
+  let service: SenhasService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SenhasService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
